d994a87dca603265d63fee314b18260438e1f259
Christian Rankovic <c.rankovic@outlook.de>
2020-02-21 11:31:11 +0100

    Add project files.

---
+++ Scrum_Group_Exercise/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Scrum_Group_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
